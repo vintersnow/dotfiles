880fe12dd563dae75947a43e03398bb06a18e2b4
izuku <ankasandora@gmail.com>
2015-02-16 22:29:07 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/bin/zsh -u
 DOTFILES="${HOME}/.dotfiles"
 if [ -d $DOTFILES -a -r $DOTFILES -a -x $DOTFILES ]; then
-  for i in $DOTFILES/.?*; do
-    [ \( -f $i -o -h $i \) -a -r $i ] && . $i
-    if [[ -f $i -a -r $i ]]; then
+  for i in .?*; do
+    if [[ -r $i ]]; then
       case $i in
         ..)
           continue;;
         .git)
           continue;;
         *)
-          ln -is "$DOTFILES/$i" $HOME;;
+          ln -is "$DOTFILES/$i" "$HOME/$i";;
       esac
     fi
   done
