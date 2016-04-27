2768cedf2a9aa7d53f5418010d8cb25d4c715c0d
izuku <ankasandora@gmail.com>
2016-04-27 10:55:17 +0900

    fix: don't clear when peco cd

--- .zsh.d/peco.zsh
+++ .zsh.d/peco.zsh
@@ -3,7 +3,7 @@
 function peco-select-history() {
   BUFFER=$(\history -n -r 1 | peco --query "$LBUFFER")
   CURSOR=$#BUFFER
-  zle clear-screen
+  zle reset-prompt
 }
 zle -N peco-select-history
 bindkey '^r' peco-select-history
