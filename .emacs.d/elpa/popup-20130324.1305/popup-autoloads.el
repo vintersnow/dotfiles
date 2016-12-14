7dcffabfd3261ce8d3914029bab9bd46a53910c8
vinter <ankasandora@gmail.com>
2016-12-14 21:30:27 +0900

    remove: .emacs.d

--- .emacs.d/elpa/popup-20130324.1305/popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (20985 62689
-;;;;;;  430769 0))
-
-;;;***
-
-(provide 'popup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-autoloads.el ends here
